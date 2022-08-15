2c784366a5857044c155440e05fcbd78c73815d5
Nevena Grujic <nevena.grujic1203@gmail.com>
2022-08-15 11:41:40 +0200

    Correction homework 12.08.2022

--- src/p12_08_2022/Zadatak2.java
+++ src/p12_08_2022/Zadatak2.java
@@ -3,7 +3,7 @@ package p12_08_2022;
 public class Zadatak2 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World2!");
+		System.out.println("Hello World2!!!!!!");
 
 	}
 
