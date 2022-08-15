527d72ab63082f7b1134d5683fad652b00d23832
Nevena Grujic <nevena.grujic1203@gmail.com>
2022-08-15 11:30:44 +0200

    Homework 12.08.2022

---
+++ src/p12_08_2022/Zadatak1.java
@@ -0,0 +1,10 @@
+package p12_08_2022;
+
+public class Zadatak1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+	}
+
+}
---
+++ src/p12_08_2022/Zadatak2.java
@@ -0,0 +1,10 @@
+package p12_08_2022;
+
+public class Zadatak2 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World2!");
+
+	}
+
+}
